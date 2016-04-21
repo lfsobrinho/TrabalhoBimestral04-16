6e19ff4b9568fbd1ef6083a3e48501e00709968f
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-04-20 22:03:15 -0300

    Implmentando a execuçao

---
+++ TrabalhoBimestral/src/br/univel/Execute.java
@@ -0,0 +1,53 @@
+package br.univel;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import br.univel.anotation.*;
+
+public class Execute extends SqlGen {
+	public Execute(){
+	}
+
+	@Override
+	protected String getCreateTable(Connection con, Object obj) {
+		
+		return null;
+	}
+
+	@Override
+	protected String getDropTable(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlInsert(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectAll(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlUpdateById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlDeleteById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
