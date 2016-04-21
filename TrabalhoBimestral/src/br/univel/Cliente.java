8ca3271ec18b5a484ecb1bd638fa7c160c158702
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-04-20 21:35:29 -0300

    Configurando a classe cliente

--- TrabalhoBimestral/src/br/univel/Cliente.java
+++ TrabalhoBimestral/src/br/univel/Cliente.java
@@ -1,7 +1,34 @@
 package br.univel;
 
+import br.univel.anotation.*;
+
+@Tabela("CAD_CLIENTE")
 public class Cliente {
-	private String name ,end , phone;
-	private int Id;
+
+	@Coluna(pk=true)
+	private int id;
+
+	@Coluna(nome="CLNOME")
+	private String nome;
+	
+	@Coluna(nome="CLENDERECO")
+	private String end;
+	
+	@Coluna(nome="CLTELEFONE")
+	private String phone;
+	
+	
+	public Cliente() {
+		this(0, null);
+	}
+
+	public Cliente(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+
 
 }
