279ae36aa4a91b95127e426a96ba1c0b18c3eb4f
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-04-21 00:42:44 -0300

    Terminando o primeiro metodo da classe execuçao

--- TrabalhoBimestral/src/br/univel/Execute.java
+++ TrabalhoBimestral/src/br/univel/Execute.java
@@ -2,8 +2,9 @@ package br.univel;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import br.univel.anotation.*;
+
+import br.univel.anotation.Coluna;
+import br.univel.anotation.Tabela;
 
 public class Execute extends SqlGen {
 	public Execute(){
@@ -27,19 +28,82 @@ public class Execute extends SqlGen {
 			Field[] atributos = cl.getDeclaredFields();
 			
 			for (int i=0; i < atributos.length; i++){
+				Field field = atributos[i];
 				
+				String nomeColuna;
+				String tipoColuna;
+				
+				if(field.isAnnotationPresent(Coluna.class)){
+					Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
+					
+					if(anotacaoColuna.nome().isEmpty()){
+						nomeColuna = field.getName().toUpperCase();
+					}else{
+						nomeColuna = anotacaoColuna.nome();				
+					}
+				}else{
+					nomeColuna = field.getName().toUpperCase();
+				}
+				
+				Class<?> tipoParametro = field.getType();
+				
+				if(tipoParametro.equals(String.class)){
+					tipoColuna = "VARCHAR(100)";
+					
+				}else{
+					if(tipoParametro.equals(int.class)){
+						tipoColuna = "INT";
+					}else{
+						tipoColuna = "DESCONHECIDO";
+					}
+					
+					if (i > 0){
+						sb.append(",");
+					}
+					
+					sb.append("\n\t").append(nomeColuna).append(' ').append(tipoColuna);
+				}
 			}
+			// Declaraçao da PK
+			
+			sb.append(",\n\tPRYMARY KEY( ");
+			
+			for (int i = 0, achou = 0; i < atributos.length; i++) {
+				Field field = atributos[i];
+				
+				if (field.isAnnotationPresent(Coluna.class)) {
+					
+					Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
+					
+					if (anotacaoColuna.pk()) {
+						
+						if (achou > 0){
+							sb.append(", ");
+						}
+						
+						if (anotacaoColuna.nome().isEmpty()) {
+							sb.append(field.getName().toUpperCase());
+						} else {
+							sb.append(anotacaoColuna.nome());
+						}
+						
+						achou++;
+					}
+				}
+				sb.append(" )");
+			}
+			sb.append("\n);");
+			
+			return sb.toString();
+			
+		}catch (SecurityException e) {
+			throw new Exception(e);
 		}
-			
-			
-			
-			
-		return null;
 	}
 
 	@Override
 	protected String getDropTable(Connection con, Object obj) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
