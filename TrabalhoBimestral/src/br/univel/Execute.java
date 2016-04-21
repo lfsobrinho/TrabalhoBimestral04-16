6067a8ab5c0f5288016b9520b1113a82c993c088
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-04-21 01:10:17 -0300

    Mais um metodo implementado

--- TrabalhoBimestral/src/br/univel/Execute.java
+++ TrabalhoBimestral/src/br/univel/Execute.java
@@ -2,6 +2,7 @@ package br.univel;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import br.univel.anotation.Coluna;
 import br.univel.anotation.Tabela;
@@ -103,14 +104,104 @@ public class Execute extends SqlGen {
 
 	@Override
 	protected String getDropTable(Connection con, Object obj) {
-		
 		return null;
+
 	}
 
 	@Override
 	protected PreparedStatement getSqlInsert(Connection con, Object obj) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Class<? extends Object> cl = obj.getClass();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		//Declarando a tabela
+		
+		String nomeTabela;
+		if(cl.isAnnotationPresent(Tabela.class)) {
+			
+			Tabela anotacaoTabela = cl.getAnnotation(Tabela.class);
+			nomeTabela = anotacaoTabela.value();
+			
+		} else {
+			nomeTabela = cl.getSimpleName().toUpperCase();
+		}
+		sb.append("INSERT INTO ").append(nomeTabela).append(" (");
+		
+		Field[] atributos = cl.getDeclaredFields();
+		
+		for (int i = 0; i < atributos.length; i++) {
+			
+			Field field = atributos[i];
+			
+			String nomeColuna;
+			
+			if (field.isAnnotationPresent(Coluna.class)) {
+				Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
+				
+				if (anotacaoColuna.nome().isEmpty()) {
+					nomeColuna = field.getName().toUpperCase();
+				} else {
+					nomeColuna = anotacaoColuna.nome();
+				}
+		
+			} else {
+				nomeColuna = field.getName().toUpperCase();
+			}
+			
+			if (i > 0) {
+				sb.append(", ");
+			}
+			
+			sb.append(nomeColuna);
+		}
+		
+		sb.append(") VALUES (");
+		
+		for (int i = 0; i < atributos.length; i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append('?');
+		}
+		sb.append(')');
+		
+		String strSql = sb.toString();
+		System.out.println("SQL GERADO: " + strSql);
+		
+		// comeca a coocar o valor do prepared statement
+		PreparedStatement ps = null;
+		try {
+			ps = con.prepareStatement(strSql);
+			
+			for (int i = 0; i < atributos.length; i++) {
+				Field field = atributos[i];
+				
+				// importante nao esquecer
+				field.setAccessible(true);
+				
+				if (field.getType().equals(int.class)) {
+					ps.setInt(i + 1, field.getInt(obj));
+					
+				}else{
+					if (field.getType().equals(String.class)) {
+						ps.setString(i + 1, String.valueOf(field.get(obj)));
+						
+					}else{
+						throw new RuntimeException("Tipo nao suportado , falta implementar");
+						
+					}
+					
+				}
+			} catch (SQLException e){
+				e.printStackTrace();
+			}catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		return ps;
+
 	}
 
 	@Override
