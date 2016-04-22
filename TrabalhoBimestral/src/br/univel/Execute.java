cab2b797a26eb93e249621f26cd2c6d9a0139a38
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-04-22 14:46:34 -0300

    Manutençao do codigo

--- TrabalhoBimestral/src/br/univel/Execute.java
+++ TrabalhoBimestral/src/br/univel/Execute.java
@@ -1,4 +1,5 @@
 package br.univel;
+
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,98 +9,104 @@ import br.univel.anotation.Coluna;
 import br.univel.anotation.Tabela;
 
 public class Execute extends SqlGen {
-	public Execute(){
+	public Execute() {
 	}
 
 	@Override
 	protected String getCreateTable(Connection con, Object obj) {
-		try{
+		try {
 			String nomeTabela;
 			Class<?> cl = obj.getClass();
 			StringBuilder sb = new StringBuilder();
-			
-			if(cl.isAnnotationPresent(Tabela.class)){
+
+			if (cl.isAnnotationPresent(Tabela.class)) {
 				Tabela anotacaoTabela = cl.getAnnotation(Tabela.class);
 				nomeTabela = anotacaoTabela.value();
-			}else{
+			} else {
 				nomeTabela = cl.getSimpleName().toUpperCase();
 			}
 			sb.append("CREATE TABLE ").append(nomeTabela).append(" (");
-			
+
 			Field[] atributos = cl.getDeclaredFields();
-			
-			for (int i=0; i < atributos.length; i++){
+
+			for (int i = 0; i < atributos.length; i++) {
 				Field field = atributos[i];
-				
+
 				String nomeColuna;
 				String tipoColuna;
-				
-				if(field.isAnnotationPresent(Coluna.class)){
+
+				if (field.isAnnotationPresent(Coluna.class)) {
 					Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
-					
-					if(anotacaoColuna.nome().isEmpty()){
+
+					if (anotacaoColuna.nome().isEmpty()) {
 						nomeColuna = field.getName().toUpperCase();
-					}else{
-						nomeColuna = anotacaoColuna.nome();				
+					} else {
+						nomeColuna = anotacaoColuna.nome();
 					}
-				}else{
+				} else {
 					nomeColuna = field.getName().toUpperCase();
 				}
-				
+
 				Class<?> tipoParametro = field.getType();
-				
-				if(tipoParametro.equals(String.class)){
+
+				if (tipoParametro.equals(String.class)) {
 					tipoColuna = "VARCHAR(100)";
-					
-				}else{
-					if(tipoParametro.equals(int.class)){
+
+				} else {
+					if (tipoParametro.equals(int.class)) {
 						tipoColuna = "INT";
-					}else{
+					} else {
 						tipoColuna = "DESCONHECIDO";
 					}
-					
-					if (i > 0){
+
+					if (i > 0) {
 						sb.append(",");
 					}
-					
+
 					sb.append("\n\t").append(nomeColuna).append(' ').append(tipoColuna);
 				}
 			}
 			// Declaraçao da PK
-			
+
 			sb.append(",\n\tPRYMARY KEY( ");
-			
+
 			for (int i = 0, achou = 0; i < atributos.length; i++) {
 				Field field = atributos[i];
-				
+
 				if (field.isAnnotationPresent(Coluna.class)) {
-					
+
 					Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
-					
+
 					if (anotacaoColuna.pk()) {
-						
-						if (achou > 0){
+
+						if (achou > 0) {
 							sb.append(", ");
 						}
-						
+
 						if (anotacaoColuna.nome().isEmpty()) {
 							sb.append(field.getName().toUpperCase());
 						} else {
 							sb.append(anotacaoColuna.nome());
 						}
-						
+
 						achou++;
 					}
 				}
 				sb.append(" )");
 			}
 			sb.append("\n);");
-			
+
 			return sb.toString();
-			
-		}catch (SecurityException e) {
-			throw new Exception(e);
+
+		} catch (SecurityException e) {
+			try {
+				throw new Exception(e);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
+		
 	}
 
 	@Override
@@ -201,7 +208,7 @@ public class Execute extends SqlGen {
 				e.printStackTrace();
 			}
 		return ps;
-
+		
 	}
 
 	@Override
